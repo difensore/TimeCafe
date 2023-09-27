5f43e6e8ba6c833e8996abc80322ffb1cd398817
Difensore <93582275+difensore@users.noreply.github.com>
2023-09-27 23:04:12 +0300

    UserController is added (#4)

---
+++ TimeCafe/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TimeCafe.Models;
+
+namespace TimeCafe.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public UserController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
