c85f27990bce32c71ce114038147b4713cfec057
difensore <aledima0209@gmail.com>
2023-12-09 19:11:27 +0200

    hotfix

---
+++ TimeCafe.DAL/Models/ViewModel/GameViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCafe.DAL.Models.ViewModel
+{
+    public class GameViewModel
+    {
+        IEnumerable<Game>? Games { get; set; }
+    }
+}
